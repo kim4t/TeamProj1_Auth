4d9e49c1d0ce5cda41a36a8582c1c9525122e434
CENTRAL\kim4t <kim4t@cmich.edu>
2021-10-10 17:46:44 -0400

    Empty file

---
+++ TeamProj_auth/src/main/java/bfs/TeamProj_auth/TeamProjAuthApplication.java
@@ -0,0 +1,13 @@
+package bfs.TeamProj_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjAuthApplication.class, args);
+	}
+
+}
